a3b6f04a47ee8d8d81996da19741fb138e35c8ba
Eliot Pickhardt <epickhardt@wisc.edu>
2024-03-13 17:17:47 -0500

    Modified Play/Pause and Skip/Previous

--- app/src/main/java/com/example/conductor/MediaControllerInterfaceActivity.java
+++ app/src/main/java/com/example/conductor/MediaControllerInterfaceActivity.java
@@ -11,7 +11,6 @@ import android.media.session.MediaController;
 import android.media.session.MediaSessionManager;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 

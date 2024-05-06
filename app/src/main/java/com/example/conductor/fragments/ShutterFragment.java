b7a96a5ba8f61c21dea849e615179ed80f5c7641
Chase Mathis <chase.mathis313@gmail.com>
2024-05-06 16:27:04 -0500

    Commented shutter fragment

--- app/src/main/java/com/example/conductor/fragments/ShutterFragment.java
+++ app/src/main/java/com/example/conductor/fragments/ShutterFragment.java
@@ -9,28 +9,37 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.example.conductor.R;
 import android.widget.ImageView;
 import android.graphics.Bitmap;
 
+import com.example.conductor.R;
+
 /**
  * A simple {@link Fragment} subclass.
+ * Use the {@link ShutterFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class ShutterFragment extends Fragment {
 
     private MediaController mediaController;
-
     private ImageView album_art;
 
+    /**
+     * Default constructor for ShutterFragment.
+     */
     public ShutterFragment() {
         // Required empty public constructor
     }
 
+    /**
+     * Constructor for ShutterFragment with media controller.
+     *
+     * @param mediaController The MediaController instance.
+     */
     public ShutterFragment(MediaController mediaController) {
         this.mediaController = mediaController;
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,30 +50,34 @@ public class ShutterFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_shutter, container, false);
-
     }
 
     @Override
     public void onResume() {
         super.onResume();
 
+        // Find and initialize album_art ImageView
         View view = getView();
         if (view != null) {
             album_art = view.findViewById(R.id.album_art);
         }
+        // Update album art
         updateAlbumArt();
     }
 
-
+    /**
+     * Updates the album art ImageView with the current media's album art.
+     */
     public void updateAlbumArt() {
         View view = getView();
+        // Check if view and media controller are not null
         if (view != null && mediaController != null) {
             MediaMetadata metadata = mediaController.getMetadata();
-            if (metadata != null) {
-                if (metadata.getBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART) != null) {
-                    Bitmap album_cover = metadata.getBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART);
-                    album_art.setImageBitmap(album_cover);
-                }
+            // Check if metadata is not null and album art is available
+            if (metadata != null && metadata.getBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART) != null) {
+                // Retrieve album art bitmap and set it to the ImageView
+                Bitmap albumCover = metadata.getBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART);
+                album_art.setImageBitmap(albumCover);
             }
         }
     }
